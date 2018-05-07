e6b0b89f094c2faf81197e47610d4c6003751e3f
Mikael DELSOL <delsol.mikael@gmail.com>
2018-05-08 00:46:17 +0200

    new test

--- tests/FileGetContentsTest.php
+++ tests/FileGetContentsTest.php
@@ -3,9 +3,15 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use WsdlToPhp\PackageGenerator\Generator\Utils;
 
 class FileGetContents extends TestCase
 {
+    public function testUtilsGetFileContentMustNotReturnFalse()
+    {
+        $this->assertNotFalse(Utils::getContentFromUrl('https://www.paypalobjects.com/wsdl/PayPalSvc.wsdl'));
+    }
+
     public function testFileGetContentsWithPayPalWsdlUrlMustNotReturnFalse()
     {
         $this->assertNotFalse(file_get_contents('https://www.paypalobjects.com/wsdl/PayPalSvc.wsdl'));
